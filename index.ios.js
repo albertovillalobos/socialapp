6a8a038eef496ec5ed295f53f3c860e9dc1c280c
Alberto Villalobos <alberto@albertovillalobos.me>
2015-07-08 21:36:43 -0700

    fetch working!

--- index.ios.js
+++ index.ios.js
@@ -14,6 +14,8 @@ var {
   TouchableHighlight
 } = React;
 
+var credentials = {};
+
 var socialapp = React.createClass({
   render: function() {
     return (
@@ -21,14 +23,59 @@ var socialapp = React.createClass({
         <Text style={styles.welcome}>
           Welcome to SocialApp
         </Text>
-        <TextInput placeholder="Username" style={styles.textField} />
-        <TextInput placeholder="Password" style={styles.passwordTextField}  secureTextEntry='YES'/>
-        <TouchableHighlight style={styles.submitButton} underlayColor='#CCC'>
+
+        <TextInput placeholder="Email" style={styles.textField} onChangeText={(text) => this.setEmail(text) }/>
+        <TextInput placeholder="Password"
+          style={styles.passwordTextField}
+          onChangeText={(text) => this.setPassword(text)}
+          secureTextEntry='YES'/>
+        <TouchableHighlight style={styles.submitButton}
+          underlayColor='#CCC'
+          onPress={() => this.submitLogin()}>
             <Text style={styles.submitText}>Submit</Text>
       	</TouchableHighlight>
       </View>
     );
+  },
+
+  setEmail: function(text) {
+    credentials.email = text;
+    console.log(credentials.email);
+
+  },
+
+  setPassword: function(text) {
+    credentials.password = text;
+    console.log(credentials.password);
+  },
+
+  submitLogin: function() {
+    console.log('credentials:', credentials);
+
+    // Auth url: http://192.241.212.180:9004/auth/local
+    var apiserver = 'http://192.241.212.180:9004/auth/local';
+
+
+    fetch(apiserver, {
+      method: 'post',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify( {
+        email: credentials.email,
+        password: credentials.password,
+      })
+    }).then( function(response) {
+      if (response.status === 200) {
+        console.log('success!');
+      }
+      else {
+        console.log('failure');
+      }
+    })
   }
+
 });
 
 var styles = StyleSheet.create({
@@ -73,7 +120,6 @@ var styles = StyleSheet.create({
     marginLeft: 20,
     marginRight: 20,
     backgroundColor: '#BBB',
-    textAlign: 'center',
     alignItems: 'center',
     borderRadius: 5,
     borderWidth: 1,
